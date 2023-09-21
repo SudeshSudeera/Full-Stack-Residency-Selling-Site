d7cdb9fcfa66b5853e55a40f0b8441668c86a797
Sudesh Sudeera <sudeshsudeera@gmail.com>
2023-09-21 20:06:48 +0530

    baseURL updated.

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 import {toast} from 'react-toastify'
 
 export const api = axios.create({
-    baseURL:"https://full-stack-residency-selling-site-delta.vercel.app/api",
+    baseURL:"https://full-stack-residency-selling-site.vercel.app/api",
 });
 
 export const getAllProperties = async() => {
